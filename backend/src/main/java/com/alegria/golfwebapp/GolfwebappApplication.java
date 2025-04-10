fab88d2bb00b700b700948ec28cb88ded846e216
Eduardo Alegria <eduardoalegria1228@gmail.com>
2025-04-10 11:09:56 -0600

    Backend agregado

---
+++ backend/src/main/java/com/alegria/golfwebapp/GolfwebappApplication.java
@@ -0,0 +1,13 @@
+package com.alegria.golfwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfwebappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alegria/golfwebapp/GolfwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alegria.golfwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolfwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
