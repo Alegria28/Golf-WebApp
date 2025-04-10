3ec7bd44cda90fa0f8630a7c6ebfe9bd681532e9
Eduardo Alegria <eduardoalegria1228@gmail.com>
2025-04-10 12:02:50 -0600

    Dependencia agregada para mysql

---
+++ backend/src/main/java/com/alegria/golfwebapp/temporal.java
@@ -0,0 +1,10 @@
+package com.alegria.golfwebapp;
+
+import com.mysql.cj.jdbc.Driver;
+
+public class temporal {
+    public static void main(String[] args) {
+        // No necesitas hacer nada con la clase Driver aquí para la compilación
+        System.out.println("Intentando referenciar la clase Driver de MySQL.");
+    }
+}
